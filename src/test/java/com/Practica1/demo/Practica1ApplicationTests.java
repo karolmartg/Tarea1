e46433d9b489420b6246812767110820fc655f43
karolmartg <karolmartgamb@hotmail.com>
2023-01-31 13:26:56 -0600

    Tarea 01.
    Esta práctica consiste en realizar un sitio web muy similar al desarrollado entre las semanas 1 a 3, pero adaptándolo a su propia versión, esto quiere decir que no puede ser la misma estructura ni contenidos hechos en clase, deben ser diferentes.

---
+++ src/test/java/com/Practica1/demo/Practica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
